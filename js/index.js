dd7d3322735b4ba701b0b335e51ccb62b55e75fd
NiteshBhanji <bhanjinitesh@gmail.com>
2021-12-02 16:42:21 +0530

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.HeroBannerContent').slick({
+        slidesToShow: 1,
+        arrows: true,
+        prevArrow: '.arrow_prev',
+        nextArrow: '.arrow_next'
+    });
+});
+
+
+$(document).ready(function () {
+    $('.ProjectSlider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: '.Projarrow_prev',
+        nextArrow: '.Projarrow_next'
+    });
+});
+
+$(document).ready(function () {
+    $('.TestimonialSlider').slick({
+        slidesToShow: 1,
+        arrows: true,
+        prevArrow: '.Testarrow_prev',
+        nextArrow: '.Testarrow_next'
+    });
+});
